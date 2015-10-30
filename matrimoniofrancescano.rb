e2877c7c3b662c978c6441688be0d62f0bd34eb2
ruvido <ruvido@gmail.com>
2015-10-30 11:53:09 +0100

    commit

--- matrimoniofrancescano.rb
+++ matrimoniofrancescano.rb
@@ -297,8 +297,9 @@ myblog=Blogdata.new( idump, iblog )
 
 [ myblog.dump_previews, myblog.dump_drafts ].each do |cdir|
 
-  # Dir["#{cdir}/{[!_]**/*,*}.md"].each do |ii|
-  Dir["#{cdir}/{**/*,*}.md"].each do |ii|
+  puts cdir
+  
+  Dir["#{cdir}/{[!_]**/*,*}.md"].each do |ii|
 
     puts '--------------------------------'
 
